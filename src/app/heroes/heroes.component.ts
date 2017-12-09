47672c22c4b110969826a86a1ed6e0640702228d
Lucas Ficarrotti <lucasfic95@gmail.com>
2017-12-08 21:40:45 -0300

    Tutorail 4 hecho

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +10,16 @@ import { Hero } from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Elise',
-    tipo: 'Jungla'
-  };
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  heroes = HEROES;
+  
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
